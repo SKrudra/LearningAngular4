13360dd89c7dc58ae09e5119fb4d9cecfe7d1333
Sandeep Kumar <skrudra12@gmail.com>
2017-12-13 20:09:02 +0530

    Initial commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,10 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    {id: 1, name: 'Jackson'},
+    {id: 2, name: 'Jacob'},
+    {id: 3, name: 'Draco'},
+    {id: 4, name: 'Moris'},
+    {id: 5, name: 'Wilson'},
+    {id: 6, name: 'Oneeye'}
+];
